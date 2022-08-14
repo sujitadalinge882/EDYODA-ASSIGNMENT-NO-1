7058c8ad4fb91fddf2d6264f0d8fe605244a429b
sujit vijay adalinge <110627737+sujitadalinge882@users.noreply.github.com>
2022-08-14 17:12:33 +0530

    Add files via upload

---
+++ EDYODA  ASSIGNMENT NO 1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# write a python program to get the fibonacci series between 0 to 50
+x,y=0,1
+while y<50:
+    print(y)
+    x,y=y,x+y
+    
+
+
+# In[ ]:
+
+
+
+
